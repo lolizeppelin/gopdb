3c6b8a71de687d0d925cac0228ad9f385c5b4c08
lolizeppelin <lolizeppelin@gmail.com>
2017-12-18 19:35:23 +0800

    配置输出

--- gopdb/config.py
+++ gopdb/config.py
@@ -7,6 +7,12 @@ def list_server_opts():
 
 
 def list_agent_opts():
+    from gopdb import common
+    group = cfg.OptGroup(common.DB)
+    CONF = cfg.CONF
+    CONF.register_group(group)
     from goperation.manager.rpc.agent.config import rpc_endpoint_opts
+    from gopdb.api.rpc.impl.config import db_opts
+    from gopdb.api.rpc.impl.mysql.config import mysql_db_opts
     cfg.set_defaults(rpc_endpoint_opts, module='gopdb.api.rpc')
-    return rpc_endpoint_opts
+    return rpc_endpoint_opts + db_opts + mysql_db_opts
