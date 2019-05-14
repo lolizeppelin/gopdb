53e5ac8a5958a33989614a5cfd740356aa1a5895
gcy <loli383>
2019-05-14 11:15:59 +0800

    简化代码

--- gopdb/api/wsgi/impl/__init__.py
+++ gopdb/api/wsgi/impl/__init__.py
@@ -695,25 +695,25 @@ class DatabaseManagerBase(object):
             _slaves_q_query = model_query(session, SchemaQuote,
                                           filter=and_(SchemaQuote.schema_id == _schema.schema_id,
                                                       SchemaQuote.database_id.in_(_slaves)))
+            quotes = {}
             # quote of slave
             slave_quotes = _slaves_q_query.all()
             for _quotes_list in (slave_quotes, _schema.quotes):
                 if _quotes_list:
-                    quotes = {}
                     for _quote in _quotes_list:
                         quotes[_quote.quote_id] = _quote.desc
+            # check quotes
+            for quote_id, desc in quotes.items():
+                if quote_id in unquotes:
+                    quotes.pop(quote_id, None)
+                if desc in ignores:
+                    quotes.pop(quote_id, None)
+            if quotes:
+                if force:
                     for quote_id, desc in quotes.items():
-                        if quote_id in unquotes:
-                            quotes.pop(quote_id, None)
-                        if desc in ignores:
-                            quotes.pop(quote_id, None)
-                    if quotes:
-                        if force:
-                            for quote in _quotes_list:
-                                if quote.quote_id in quotes.keys():
-                                    LOG.warning('Quote %d: [%s] ignore' % (quote.quote_id, quote.desc))
-                        if not force:
-                            raise exceptions.AcceptableSchemaError('Schema in quote, can not be delete')
+                        LOG.warning('Quote %d: [%s] force delete' % (quote_id, desc))
+                if not force:
+                    raise exceptions.AcceptableSchemaError('Schema in quote, can not be delete')
             with self._delete_schema(session, _database, _schema, **kwargs) as address:
                 host = address[0]
                 port = address[1]
